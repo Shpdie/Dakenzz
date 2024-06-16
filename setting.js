2056a05ac5670e546bb999b37bfa552c47234ef1
shpdie <shpvdie@gmail.com>
2024-06-16 17:03:19 +0700

    Dakenzz

---
+++ setting.js
@@ -0,0 +1,40 @@
+module.exports = {
+  name: "DakenzBot",
+  version: "1.3.0",
+  number: "6283848565087",
+  owner: {
+    name: "Adam",
+    number: "6281214557752",
+    whatsapp: "6281214557752@s.whatsapp.net",
+    telegram: "-",
+    youtube: "-",
+    instagram: "https://instagram.com/dmssrdn",
+  },
+  author: {
+    name: "Adam",
+    number: "6281214557752",
+    whatsapp: "6281214557752@s.whatsapp.net",
+    telegram: "-",
+    youtube: "-",
+    instagram: "https://instagram.com/dmssrdn",
+  },
+  features: {
+    antiCall: {
+      status: true,
+      block: false,
+    },
+    selfMode: true,
+    broadcast: {
+      text: "",
+      limit: 9999,
+      jeda: 5000,
+      filterContact: false,
+    },
+    pushContacts: {
+      text: "",
+      limit: 9999,
+      jeda: 5000,
+      filterContacts: false,
+    },
+  },
+};
